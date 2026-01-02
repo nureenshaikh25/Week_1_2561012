e17b4564b68a9fd33749fbba9993f8b6f218afbc
nureenshaikh25 <nureenshaikh07@gmail.com>
2026-01-02 19:21:20 +0530

    Add initial shop item management system implementation
    This file contains the initial implementation of a shop item management system, including features for product details, total price calculation, and product search.

---
+++ shop.c
@@ -0,0 +1,14 @@
+# Shop Item Management System (C)
+
+## Description
+This project is a Week 1 GitHub task for BCS I.
+It manages shop item records using C programming.
+
+## Features
+- Accepts details of 5 products
+- Calculates total price
+- Displays product details
+- Searches product by Product ID
+
+## Language Used
+C Programming
